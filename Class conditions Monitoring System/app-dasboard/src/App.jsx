f18c52d830a91b532b5cda332671b866aa798422
Elikplim-N <ekn2526@gmail.com>
2024-08-26 09:52:50 +0000

    changed card labels

--- Class conditions Monitoring System/app-dasboard/src/App.jsx
+++ Class conditions Monitoring System/app-dasboard/src/App.jsx
@@ -51,7 +51,7 @@ function App() {
           <div className="col-md-3">
             <div className={`card mb-4 ${checkThreshold(sensorVals.sensor1) ? "bg-danger" : ""}`}>
               <div className="card-body">
-                <h2 className="card-title">Temperature Sensor</h2>
+                <h2 className="card-title">Temperature</h2>
                 <p className="card-text" id="t1">
                   {sensorVals.sensor1.value !== null
                     ? `${sensorVals.sensor1.value} ${sensorVals.sensor1.unit}`
@@ -69,7 +69,7 @@ function App() {
           <div className="col-md-3">
             <div className={`card mb-4 ${checkThreshold(sensorVals.sensor2) ? "bg-danger" : ""}`}>
               <div className="card-body">
-                <h2 className="card-title">Humidity Sensor</h2>
+                <h2 className="card-title">Humidity</h2>
                 <p className="card-text" id="t2">
                   {sensorVals.sensor2.value !== null
                     ? `${sensorVals.sensor2.value} ${sensorVals.sensor2.unit}`
@@ -87,7 +87,7 @@ function App() {
           <div className="col-md-3">
             <div className={`card mb-4 ${checkThreshold(sensorVals.sensor3) ? "bg-danger" : ""}`}>
               <div className="card-body">
-                <h2 className="card-title">Air Quality Sensor</h2>
+                <h2 className="card-title">Air Quality</h2>
                 <p className="card-text" id="t3">
                   {sensorVals.sensor3.value !== null
                     ? `${sensorVals.sensor3.value} ${sensorVals.sensor3.unit}`
@@ -105,7 +105,7 @@ function App() {
           <div className="col-md-3">
             <div className={`card mb-4 ${checkThreshold(sensorVals.sensor4) ? "bg-danger" : ""}`}>
               <div className="card-body">
-                <h2 className="card-title">Sound Sensor</h2>
+                <h2 className="card-title">Noise Level</h2>
                 <p className="card-text" id="t4">
                   {sensorVals.sensor4.value !== null
                     ? `${sensorVals.sensor4.value} ${sensorVals.sensor4.unit}`
